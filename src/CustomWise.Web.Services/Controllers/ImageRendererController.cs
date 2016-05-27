06a7c98d80292105040e2eb9ed7328dca74b1f46
Robert Sweeney <robert.sweeney@outlook.com>
2016-05-27 16:14:25 -0700

    adding the renderer controller

---
+++ src/CustomWise.Web.Services/Controllers/ImageRendererController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CustomWise.Web.Services.Controllers.Base;
+using Sophcon.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CustomWise.Web.Services.Controllers
+{
+    [RoutePrefix("api/customwise/configuration/renderer")]
+    public class ImageRendererController : BaseController
+    {
+        public ImageRendererController()
+            : base(DataContextFactory.CreateDataContextFactory().GetContext(), AutoMapperFactory.CreateAutoMapperConfigProviderInstance(), AutoMapperFactory.CreateAutoMapperMapperInstance())
+        { }
+
+        public ImageRendererController(IDbContext context, IConfigurationProvider autoMapperConfigProvier, IMapper mapper)
+            : base (context, autoMapperConfigProvier, mapper)
+        { }
+    }
+}
