7c637dcd7ca696941f378555c5041c9ca37596a2
Yer Yang <yangyer81@gmail.com>
2016-06-09 01:32:58 -0700

    Updated Sophcon Nuget Pkgs, fixed any errors caused by update.

--- src/CustomWise.Web.Services/SaveEvents/SetCreatedModifiedDataPreSaveEvent.cs
+++ src/CustomWise.Web.Services/SaveEvents/SetCreatedModifiedDataPreSaveEvent.cs
@@ -15,13 +15,18 @@ namespace CustomWise.Web.Services.Controllers.SaveEvents {
             _userName = !string.IsNullOrWhiteSpace(userName) ? userName : "system";
         }
 
-        public override void PreSaveHandler(IEnumerable<DataEntityWrapper> entries) {
+        protected override void _ExecutePreSave(IEnumerable<DataEntityWrapper> entries) {
             foreach (var entry in entries) {
                 entry.Entity.CreatedBy = entry.State == DataEntityState.Added ? _userName : entry.Entity.CreatedBy;
                 entry.Entity.CreatedDate = entry.State == DataEntityState.Added ? DateTime.Now : entry.Entity.CreatedDate;
                 entry.Entity.ModifiedBy = entry.State.In(DataEntityState.Modified, DataEntityState.Added) ? _userName : entry.Entity.ModifiedBy;
                 entry.Entity.ModifiedDate = entry.State.In(DataEntityState.Modified, DataEntityState.Added) ? DateTime.Now : entry.Entity.ModifiedDate;
             }
+        }
+
+        protected override void _PreSaveError(SaveException thrownException) {
+            Token.SetExitAll();
+            Token.SetException(thrownException);
         }
     }
 }
